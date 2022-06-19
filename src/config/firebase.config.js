245a6299e66d67109a8fd0e256a67aa3097c1d84
Agustin cristobo <75025925+Fr33yr@users.noreply.github.com>
2022-06-19 15:14:40 -0300

    Update firebase.config.js

--- src/config/firebase.config.js
+++ src/config/firebase.config.js
@@ -4,14 +4,7 @@ import { getAuth } from 'firebase/auth'
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBirv-OkAsea5NN9_Ryzrk8uJFlZECFHB0",
-    authDomain: "arduino-temp-79cca.firebaseapp.com",
-    databaseURL: "https://arduino-temp-79cca-default-rtdb.firebaseio.com",
-    projectId: "arduino-temp-79cca",
-    storageBucket: "arduino-temp-79cca.appspot.com",
-    messagingSenderId: "502629281681",
-    appId: "1:502629281681:web:920dc5a7699ced93d9f430",
-    measurementId: "G-R31229N9EF"
+    //Firebase Config goes here
 };
 
 const app = initializeApp(firebaseConfig);
